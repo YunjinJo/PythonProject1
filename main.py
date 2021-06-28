623901408cae29d945b8e8887c24663f77454f2b
YunjinJo <black44jo@naver.com>
2021-06-28 10:19:18 +0900

    격자배치

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ from tkinter import*
 window = Tk()
 l1 = Label(window, text = "달러")
 l2 = Label(window, text = '원')
-l1.pack() #압축배치
-l2.pack() #압축배치
+l1.grid(row=0, column =0) #격자배치 0행 0열
+l2.grid(row=1, column =0) #격자배치 1행 0열
 
 e1 = Entry(window)
 e2 = Entry(window)
-e1.pack()
-e2.pack()
+e1.grid(row=0, column =1) #격자배치 0행 1열
+e2.grid(row=1, column =1) #격자배치 1행 1열
 
 
 b1 = Button(window,text = '달러 -> 원') #button 이라는 이름의 버튼 위젯을 window에 생성
 b2 = Button(window,text = '원 -> 달러')
-b1.pack() #압축배치
-b2.pack()
+b1.grid(row=2, column =0)
+b2.grid(row=2, column =1)
 window.mainloop()
