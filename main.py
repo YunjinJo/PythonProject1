31b5f3f11828826c0e8b283c7762639edafea9ea
YunjinJo <black44jo@naver.com>
2021-06-28 10:32:44 +0900

    위젯 색상 폰트 변경

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def process():
     e2.insert(0, str(dollar*1200))
 
 window = Tk()
-l1 = Label(window, text = "달러")
-l2 = Label(window, text = '원')
+l1 = Label(window, text = "달러", font = 'helvetica 16 italic')
+l2 = Label(window, text = '원', font = 'helvetica 16 italic')
 l1.grid(row=0, column =0) #격자배치 0행 0열
 l2.grid(row=1, column =0) #격자배치 1행 0열
 
-e1 = Entry(window)
-e2 = Entry(window)
+e1 = Entry(window, bg = 'yellow', fg = 'black')
+e2 = Entry(window, background = 'blue', foreground = 'white')
 e1.grid(row=0, column =1) #격자배치 0행 1열
 e2.grid(row=1, column =1) #격자배치 1행 1열
 
@@ -25,4 +25,6 @@ b1 = Button(window,text = '달러 -> 원', command = process) #button 이라는 
 b2 = Button(window,text = '원 -> 달러')
 b1.grid(row=2, column =0)
 b2.grid(row=2, column =1)
+b1.configure(font = 'helvetica 12')
+b2["bg"] = "green"
 window.mainloop()
