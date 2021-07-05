75a9d640a9e54044d1576f62602f356d1c04c9da
YunjinJo <black44jo@naver.com>
2021-07-05 10:58:25 +0900

    캐릭터 애니메이션

--- Day6/0302_1.py
+++ Day6/0302_1.py
@@ -12,13 +12,18 @@ def key_up(e):  #키를 눌렀다 뗐을 때 실행되는 함수
     global koff
     koff = True     #눌렀다 뗐을 때 설정
 
-DIR_UP = 0        #캐릭터 방향 정의 변수 (위쪽)
-DIR_DOWN = 1      #캐릭터 방향 정의 변수 (아래쪽)
-DIR_LEFT = 2      #캐릭터 방향 정의 변수 (왼쪽)
-DIR_RIGHT = 3     #캐릭터 방향 정의 변수 (오른쪽)
+DIR_UP = 0              #캐릭터 방향 정의 변수 (위쪽)
+DIR_DOWN = 1            #캐릭터 방향 정의 변수 (아래쪽)
+DIR_LEFT = 2            #캐릭터 방향 정의 변수 (왼쪽)
+DIR_RIGHT = 3           #캐릭터 방향 정의 변수 (오른쪽)
+ANIMATION = [0,1,0,2]   #애니메이션 번호 정의 리스트, 3장의 스프라이트 이미지를 0번 -> 1번 -> 0번 -> 2번 순서대로 표시
+
+tmr = 0 #타이머
 
 pen_x = 90        #펜펜의 x좌표
 pen_y = 90        #펜펜의 y좌표
+pen_d = 0         #펜펜의 방향
+pen_a = 0         #펜펜의 이미지 번호
 
 
 map_data = [                            #2차원 9행x12열 리스트 미로 데이터 정의
@@ -38,7 +43,7 @@ def draw_screen():          #미로 그리기 함수
     for y in range(9):      #y=0,1,2,...,8(9행)
         for x in range(12): #x=0,1,2,...,11(12열)
             canvas.create_image(x*60+30,y*60+30,image=img_bg[map_data[y][x]], tag='SCREEN')   #60x60 이미지 칩의 중심이 (30,30)이므로
-    canvas.create_image(pen_x,pen_y,image=img_pen,tag='SCREEN') #펜펜 이미지 표시
+    canvas.create_image(pen_x,pen_y,image=img_pen[pen_a],tag='SCREEN') #펜펜 이미지 표시
 
 def check_wall(cx, cy, di): #각 방향에 벽 존재 여부 확인 존재하면 True 리턴
     chk = False             #chk 변수에 False 할당
@@ -66,22 +71,28 @@ def check_wall(cx, cy, di): #각 방향에 벽 존재 여부 확인 존재하면
 
 
 def move_penpen():
-    global pen_x,pen_y
+    global pen_x,pen_y, pen_d, pen_a
     if key == 'Up':                                 #key symbol 중에서 'Up' 위쪽 화살표 눌렀을 때
-        if check_wall(pen_x,pen_y,DIR_UP) == False: #위쪽 방향이 벽인지 검사
+        pen_d = DIR_UP
+        if check_wall(pen_x,pen_y,pen_d) == False: #위쪽 방향이 벽인지 검사
             pen_y = pen_y -60                       #한번에 60픽셀 이동
     if key == 'Down':
-        if check_wall(pen_x, pen_y, DIR_DOWN) == False:
+        pen_d = DIR_DOWN
+        if check_wall(pen_x, pen_y, pen_d) == False:
             pen_y = pen_y + 60
     if key == 'Left':
-        if check_wall(pen_x,pen_y,DIR_LEFT) == False:
+        pen_d = DIR_LEFT
+        if check_wall(pen_x,pen_y,pen_d) == False:
             pen_x = pen_x -60
     if key == 'Right':
-        if check_wall(pen_x,pen_y,DIR_RIGHT) == False:
+        pen_d = DIR_RIGHT
+        if check_wall(pen_x,pen_y,pen_d) == False:
             pen_x = pen_x +60
+    pen_a = pen_d *3 + ANIMATION[tmr % 4]   #pen_d (0,1,2,3) * 3 -> (0,3,6,9), ANIMATION[0...3] -> 0,1,0,2
 
 def main():
-    global key, koff
+    global key, koff, tmr
+    tmr = tmr + 1
     draw_screen()
     move_penpen()           #펜펜 이동
     if koff == True:        #koff가 True이면 (키를 눌렀다 뗐으면)
@@ -100,7 +111,20 @@ img_bg = [
     tkinter.PhotoImage(file='Chapter3/image_penpen/chip02.png'),    #방
     tkinter.PhotoImage(file='Chapter3/image_penpen/chip03.png')     #캔디
 ]
-img_pen = tkinter.PhotoImage(file='Chapter3/image_penpen/pen03.png')    #펜펜 이미지 로딩
+img_pen = [
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen00.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen01.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen02.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen03.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen04.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen05.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen06.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen07.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen08.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen09.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen10.png'),
+    tkinter.PhotoImage(file='Chapter3/image_penpen/pen11.png')
+]    #펜펜 이미지 리스트
 root.bind('<KeyPress>',key_down)    #키를 눌렀을 때 실행할 함수 key_down 지정
 root.bind('<KeyRelease>',key_up)    #키를 눌렀다 뗐을 때 실행할 함수 key_up 지정
 main()
